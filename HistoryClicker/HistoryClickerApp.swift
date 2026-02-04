c2319dec931e1f410b06fabe2f46223cec164522
Alexey <lexasp.it@gmail.com>
2026-02-04 11:49:51 +0200

    Initial Commit

---
+++ HistoryClicker/HistoryClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryClickerApp.swift
+//  HistoryClicker
+//
+//  Created by Alexey Vaver on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HistoryClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
